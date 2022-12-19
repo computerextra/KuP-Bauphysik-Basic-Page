90dc337206b40d36490237f6aa111c187310bf0f
computerextra <81916339+computerextra@users.noreply.github.com>
2022-12-19 17:35:03 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -40,17 +40,71 @@ function App() {
           </Col>
         </Row>
       </Container>
+      {/* Impressum */}
       <Modal show={showImpressum} onHide={handleCloseImpressum}>
         <Modal.Header closeButton>
           <Modal.Title>Impressum</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+          <p>
+            K&amp;P Bauphysik GmbH
+            <br />
+            Ederweg 4-6
+            <br />
+            34277 Fuldabr&uuml;ck
+          </p>
+
+          <p>
+            <strong>Vertreten durch:</strong>
+            <br />
+            Dipl.-Ing. Marc Klatecki
+          </p>
+
+          <h2>Kontakt</h2>
+          <p>
+            Telefon: 0561/94990431
+            <br />
+            E-Mail: marc [PUNKT] klatecki [AT] kp-bauphysik [PUNKT] de
+          </p>
+
+          <h2>EU-Streitschlichtung</h2>
+          <p>
+            Die Europ&auml;ische Kommission stellt eine Plattform zur
+            Online-Streitbeilegung (OS) bereit:{" "}
+            <a
+              href="https://ec.europa.eu/consumers/odr/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://ec.europa.eu/consumers/odr/
+            </a>
+            .<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.
+          </p>
+
+          <h2>
+            Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+          </h2>
+          <p>
+            Wir sind nicht bereit oder verpflichtet, an
+            Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
+            teilzunehmen.
+          </p>
+
+          <p>
+            Quelle:{" "}
+            <a href="https://www.e-recht24.de/impressum-generator.html">
+              https://www.e-recht24.de/impressum-generator.html
+            </a>
+          </p>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseImpressum}>
             Schließen
           </Button>
         </Modal.Footer>
       </Modal>
+      {/* Datenschutz */}
       <Modal show={showDatenschutz} onHide={handleCloseDatenschutz}>
         <Modal.Header closeButton>
           <Modal.Title>Datenschutz</Modal.Title>
